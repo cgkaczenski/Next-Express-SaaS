d62097231e6ec78993997c600112fd70c3089a0a
cgkaczenski <kzpulaski@gmail.com>
2023-07-25 19:32:26 -0500

    disable prefetch so page reload is unneeded

--- app/src/components/menu-links.tsx
+++ app/src/components/menu-links.tsx
@@ -37,6 +37,7 @@ export default function MenuLinks() {
                   active ? "bg-gray-100" : "",
                   "block px-4 py-2 text-sm text-gray-700"
                 )}
+                prefetch={false}
               >
                 Your Profile
               </Link>
