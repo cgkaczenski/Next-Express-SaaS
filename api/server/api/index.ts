082114b92ebf98842e9c5ab8db64434eade74385
cgkaczenski <kzpulaski@gmail.com>
2023-08-02 23:16:52 -0500

    protect api from cross user requests

--- api/server/api/index.ts
+++ api/server/api/index.ts
@@ -14,25 +14,29 @@ async function checkSession(req, res, next) {
   if (req && req.headers) {
     authHeader = req.headers["authorization"];
   }
+  if (!authHeader) {
+    console.log("No authorization header");
+    return res.status(401).json({ error: "No authorization header" });
+  }
+  const sessionToken = authHeader.split(" ")[1] as string;
+  let session;
   try {
-    if (!authHeader) {
-      return res.status(401).json({ error: "No authorization header" });
-    }
-    const sessionToken = authHeader.split(" ")[1] as string;
-    const session = await SessionService.getSession({
+    session = await SessionService.getSession({
       sessionToken,
     });
-    if (!session || session.expires < new Date()) {
-      return res.status(401).json({ error: "Invalid session token" });
-    }
-    next();
   } catch (err) {
-    next(err);
+    console.log("Error getting session", err);
   }
+  if (!session || session.expires < new Date()) {
+    return res.status(401).json({ error: "Invalid session token" });
+  }
+  req.session = session;
+  next();
 }
 
 export default function api(server: express.Express) {
   server.use(checkSession);
-  server.use("/api/v1/public", publicExpressRoutes, handleError);
-  server.use("/api/v1/team-member", teamMemberExpressRoutes, handleError);
+  server.use("/api/v1/public", publicExpressRoutes);
+  server.use("/api/v1/team-member", teamMemberExpressRoutes);
+  server.use(handleError);
 }
--- api/server/api/public.ts
+++ api/server/api/public.ts
@@ -1,16 +1,18 @@
 import * as express from "express";
-
 import UserService from "../services/User";
 
 const router = express.Router();
 
-router.post("/get-user-by-email", async (req, res, next) => {
+router.post("/get-user-by-email", async (req: any, res, next) => {
   console.log("Express route: /get-user-by-email");
 
   try {
     const { email } = req.body;
 
     const user = await UserService.getUserByEmail({ email });
+    if (user.id !== req.session.userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
     res.json({ user });
   } catch (err) {
     next(err);
@@ -23,7 +25,9 @@ router.post("/user/update-profile", async (req: any, res, next) => {
   try {
     const { email, name, avatarUrl } = req.body;
     const user = await UserService.getUserByEmail({ email });
-
+    if (user.id !== req.session.userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
     const updatedUser = await UserService.updateProfile({
       userId: user.id,
       name,
